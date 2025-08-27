80e64562f5fb3f7377b71548cb7f4b899c499ec4
Wilhelm Wijkander <wilhelm.wijkander@bahnhof.net>
2025-08-27 21:23:09 +0200

    rework dhcp.go and add dhcp6.go

--- dhcp.go
+++ dhcp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"time"
 
 	dhcp "github.com/insomniacslk/dhcp/dhcpv4"
@@ -12,52 +13,62 @@ import (
 
 var (
 	dhcpRequests = promauto.NewCounter(prometheus.CounterOpts{
-			Name: "observer_dhcp_requests",
-			Help: "Total number of sent DHCP requests",
+		Name: "observer_dhcp_requests",
+		Help: "Total number of sent DHCPv4 requests",
 	})
 	dhcpOffers = promauto.NewCounter(prometheus.CounterOpts{
-			Name: "observer_dhcp_offers",
-			Help: "Total number of received DHCP offers",
+		Name: "observer_dhcp_offers",
+		Help: "Total number of received DHCPv4 offers",
 	})
 	dhcpFailures = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "observer_dhcp_failures",
-		Help: "Total number of failed DHCP handshakes",
+		Help: "Total number of failed DHCPv4 handshakes",
 	})
-	dchpHandshakeLatency = prometheus.NewGauge(prometheus.GaugeOpts{
+	dhcpHandshakeLatency = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "observer_dhcp_latency",
-		Help: "Time between dhcp request and dhcp offer.",
+		Help: "Time between DHCPv4 request and offer.",
 	})
-	dchpLeaseTime = prometheus.NewGauge(prometheus.GaugeOpts{
+	dhcpLeaseTime = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "observer_dhcp_lease_time",
-		Help: "Time until DHCP lease will be renewed.",
+		Help: "Time until DHCPv4 lease will be renewed.",
 	})
 )
 
 func init() {
-	prometheus.MustRegister(dchpHandshakeLatency)
-	prometheus.MustRegister(dchpLeaseTime)
+	prometheus.MustRegister(dhcpHandshakeLatency)
+	prometheus.MustRegister(dhcpLeaseTime)
 }
 
-func sampleDhcp(iface string, verbose bool) {
+func sampleDhcp(iface string, verbose bool) (error, net.IP, int) {
 	dhcpRequests.Inc()
 	client := client.NewClient()
 	start := time.Now()
 	conversation, err := client.Exchange(iface)
 	if err != nil {
 		dhcpFailures.Inc()
-		fmt.Printf("DHCP Failed with %s.\n", err)
-		return
+		return err, nil, 0
 	}
-	dchpHandshakeLatency.Set(time.Since(start).Seconds())
+	dhcpHandshakeLatency.Set(time.Since(start).Seconds())
+
+	var yourIPAddr net.IP
+	var prefixBits int
 	for _, packet := range conversation {
-	 	if packet.MessageType() ==  dhcp.MessageTypeOffer {
-			dhcpOffers.Inc()
-		}
-		if packet.Options.Has(dhcp.OptionIPAddressLeaseTime) {
-			dchpLeaseTime.Set(packet.IPAddressLeaseTime(0).Seconds())
-		}
 		if verbose {
 			fmt.Println(packet.Summary())
 		}
+
+		if packet.MessageType() == dhcp.MessageTypeOffer {
+			dhcpOffers.Inc()
+		}
+
+		if packet.MessageType() == dhcp.MessageTypeAck {
+			if packet.Options.Has(dhcp.OptionIPAddressLeaseTime) {
+				dhcpLeaseTime.Set(packet.IPAddressLeaseTime(0).Seconds())
+			}
+
+			prefixBits, _ = packet.SubnetMask().Size()
+			yourIPAddr = packet.YourIPAddr
+		}
 	}
+	return nil, yourIPAddr, prefixBits
 }
---
+++ dhcp6.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	dhcp "github.com/insomniacslk/dhcp/dhcpv6"
+	client "github.com/insomniacslk/dhcp/dhcpv6/client6"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	dhcp6Requests = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "observer_dhcp6_requests",
+		Help: "Total number of sent DHCPv6 requests",
+	})
+	dhcp6Replies = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "observer_dhcp6_offers",
+		Help: "Total number of received DHCPv6 replies(offers)",
+	})
+	dhcp6Failures = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "observer_dhcp6_failures",
+		Help: "Total number of failed DHCPv6 handshakes",
+	})
+	dhcp6HandshakeLatency = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "observer_dhcp6_latency",
+		Help: "Time between DHCPv6 request and offer.",
+	})
+	dhcp6Lifetime = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "observer_dhcp6_lifetime",
+		Help: "Duration that DHCPv6 obtained address is valid.",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(dhcp6HandshakeLatency)
+	prometheus.MustRegister(dhcp6Lifetime)
+}
+
+func sampleDhcp6(iface string, verbose bool) (error, net.IP, int) {
+	dhcp6Requests.Inc()
+	client := client.NewClient()
+	start := time.Now()
+	conversation, err := client.Exchange(iface)
+	if err != nil {
+		dhcp6Failures.Inc()
+		return err, nil, 0
+	}
+	dhcp6HandshakeLatency.Set(time.Since(start).Seconds())
+
+	var yourIPAddr net.IP
+	var prefixBits int = 128
+	for _, packet := range conversation {
+		if verbose {
+			fmt.Println(packet.Summary())
+		}
+
+		message, err := packet.GetInnerMessage()
+		if err != nil {
+			dhcp6Failures.Inc()
+			return err, nil, 0
+		}
+
+		if message.MessageType == dhcp.MessageTypeReply {
+			dhcp6Replies.Inc()
+
+			iaNa := message.Options.OneIANA()
+			naAddr := *iaNa.Options.OneAddress()
+
+			dhcp6Lifetime.Set(naAddr.ValidLifetime.Seconds())
+			yourIPAddr = naAddr.IPv6Addr
+		}
+	}
+	return nil, yourIPAddr, prefixBits
+}
