992db2b9bd8a28da76715de4b7bc4746d6e6a67b
Wilhelm Wijkander <wilhelm.wijkander@bahnhof.net>
2025-08-27 21:19:02 +0200

    switch icmp.go to use pro-bing, change exposed metrics and select outgoing ping interface

--- icmp.go
+++ icmp.go
@@ -1,54 +1,80 @@
 package main
 
 import (
-	"github.com/go-ping/ping"
+	probing "github.com/prometheus-community/pro-bing"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 var (
-	icmpRequests = promauto.NewCounterVec(prometheus.CounterOpts{
-			Name: "observer_icmp_requests",
-			Help: "Total number of sent ICMP requests",
+	icmpSetupFailures = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "observer_icmp_setup_failures",
+		Help: "Total number of failed ICMP tests due to errors setting up the measurement",
 	}, []string{"target"})
-	icmpFailures = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "observer_icmp_failures",
-		Help: "Total number of failed ICMP requests",
-	}, []string{"target"})
-	icmpRtt = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "observer_icmp_rtt",
-		Help: "RTT of ICMP request.",
-	}, []string{"target"})
+	icmpAvgRtt = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "observer_icmp_avg_rtt",
+		Help: "Average round trip time of ICMP request.",
+	}, []string{"target", "resolved_addr"})
+	icmpSentRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "observer_sent_icmp_requests",
+		Help: "Total number of sent ICMP echo requests",
+	}, []string{"target", "resolved_addr"})
+	icmpReceivedRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "observer_received_icmp_requests",
+		Help: "Total number of received ICMP echo replies",
+	}, []string{"target", "resolved_addr"})
 )
 
 func init() {
-	prometheus.MustRegister(icmpRtt)
+	prometheus.MustRegister(icmpAvgRtt)
 }
 
 func sampleIcmp(targets []string, count int) {
 	for _, target := range targets {
-		icmpRequests.With(prometheus.Labels{
-			"target": target,
-		}).Add(float64(count))
-		f := icmpFailures.With(prometheus.Labels{
+		f := icmpSetupFailures.With(prometheus.Labels{
 			"target": target,
 		})
 		f.Add(float64(0))
-		pinger, err := ping.NewPinger(target)
-		pinger.SetPrivileged(true)
+		pinger, err := probing.NewPinger(target)
 		if err != nil {
 			f.Inc()
 			return
 		}
+		pinger.SetPrivileged(true)
 		pinger.Count = count
-		pinger.Run()
-		stats := pinger.Statistics()
-		if stats.PacketsSent != stats.PacketsRecv {
-			f.Inc()
+		pinger.InterfaceName = *iface
+
+		var af string
+		if !*disable4 {
+			af = "ip6"
+		} else if !*disable6 {
+			af = "ip4"
+		} else {
+			af = "ip"
 		}
-		icmpRtt.With(prometheus.Labels{
-			"target": target,
+		pinger.SetNetwork(af)
+
+		err = pinger.Run()
+		if err != nil {
+			f.Inc()
+			return
+		}
+
+		stats := pinger.Statistics()
+
+		icmpSentRequests.With(prometheus.Labels{
+			"target":        target,
+			"resolved_addr": stats.IPAddr.String(),
+		}).Add(float64(stats.PacketsSent))
+
+		icmpReceivedRequests.With(prometheus.Labels{
+			"target":        target,
+			"resolved_addr": stats.IPAddr.String(),
+		}).Add(float64(stats.PacketsRecv))
+
+		icmpAvgRtt.With(prometheus.Labels{
+			"target":        target,
+			"resolved_addr": stats.IPAddr.String(),
 		}).Set(stats.AvgRtt.Seconds())
 	}
-	
 }
